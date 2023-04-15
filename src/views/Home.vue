72c6731dc495a5e1f1c9adb3e9c413edc8deb726
luisfelipebp <luisfelipebarbosa2013@gmail.com>
2023-04-15 20:22:12 -0300

    removendo titulo home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,34 @@
 <template>
   <section>
     <div v-if="loading" class="loading"></div>
-    <div>
-      <h1 class="main-title">SALES</h1>
-      <div class="home-container">
-        <div class="img-box">
-          <img
-            src="@/assets/roupas.jpg"
-            alt="roupas"
-            v-on:load="carregarImagem"
-          />
-        </div>
-        <div class="home-text" v-if="image">
-          <p>
-            Welcome to our online sales store! Here, you will find a wide
-            variety of products, from clothing and fashion accessories to
-            electronics and home goods. Our page is updated daily with new
-            arrivals and promotions so that you can find the best products for
-            the best prices
-          </p>
-          <p>
-            To make your purchase easier, we offer several payment methods, such
-            as credit card, debit card, and bank slip. In addition, we have an
-            easy and transparent exchange and return policy to ensure your
-            satisfaction with the products purchased.
-          </p>
-          <p>
-            Our customer service team is always ready to help you with any
-            questions or issues that may arise during your purchases. We are
-            committed to the quality and satisfaction of our customers, so we
-            strive to offer the best service and products.
-          </p>
-          <router-link to="/products" class="btn-produtos"
-            >Products</router-link
-          >
-        </div>
+    <div class="home-container">
+      <div class="img-box">
+        <img
+          src="@/assets/roupas.jpg"
+          alt="roupas"
+          v-on:load="carregarImagem"
+        />
+      </div>
+      <div class="home-text" v-if="image">
+        <p>
+          Welcome to our online sales store! Here, you will find a wide variety
+          of products, from clothing and fashion accessories to electronics and
+          home goods. Our page is updated daily with new arrivals and promotions
+          so that you can find the best products for the best prices
+        </p>
+        <p>
+          To make your purchase easier, we offer several payment methods, such
+          as credit card, debit card, and bank slip. In addition, we have an
+          easy and transparent exchange and return policy to ensure your
+          satisfaction with the products purchased.
+        </p>
+        <p>
+          Our customer service team is always ready to help you with any
+          questions or issues that may arise during your purchases. We are
+          committed to the quality and satisfaction of our customers, so we
+          strive to offer the best service and products.
+        </p>
+        <router-link to="/products" class="btn-produtos">Products</router-link>
       </div>
     </div>
   </section>
@@ -62,7 +56,7 @@ export default {
 section {
   max-width: 1400px;
   margin: 0 auto;
-  padding: 50px 50px 0 50px;
+  padding: 100px 50px 0 50px;
 }
 .home-container {
   display: flex;
